30f18aaa879d1f62edcbdf1f9708f9e8bcbfe820
Renan Passos <renanpassos@poli.ufrj.br>
2018-01-19 11:51:24 -0200

    Adiciona programa Java que calcula trias de resistores de valor comercial (série E12) com uma certa relação de resistências para fim específico, apresentando um parâmetro de desvio com relação ao valor ideal.

---
+++ Combinações de Resistores/src/Resistores.java
@@ -0,0 +1,38 @@
+
+public class Resistores {
+
+	final static float [] serieE12 = {10, 12, 15, 18, 22, 27, 33, 39, 47, 56, 68, 82, 100, 120, 150, 180, 220, 270, 330};  
+	final static float razaoIdeal1 = (float) (0.8/5.0);
+	final static float razaoIdeal2 = (float) (3.0/5.0);
+	final static float tolerancia = (float) 0.01;	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=0 ; i<19 ; i++) {
+			for (int j=0 ; j<19 ; j++) {
+				for (int k=0 ; k<19 ; k++) {
+					float soma = serieE12[i] + serieE12[j] + serieE12[k];
+					float val1 = (serieE12[i]/soma) / razaoIdeal1;
+					float val2 = (serieE12[k]/soma) / razaoIdeal2;
+					
+					if ( (1 - tolerancia) <= val1 && val1 <= (1 + tolerancia)
+					  && (1 - tolerancia) <= val2 && val2 <= (1 + tolerancia) ) {
+						System.out.print(serieE12[i]);
+						System.out.print(" ; ");
+						System.out.print(serieE12[j]);
+						System.out.print(" ; ");
+						System.out.print(serieE12[k]);
+						System.out.print(" | ");
+						System.out.print(val1);
+						System.out.print(" ; ");
+						System.out.print(val2);
+						System.out.print(" ; ");
+						System.out.print(soma);
+						System.out.println(" ohm");
+					}
+				}
+			}
+		}
+	}
+
+}
