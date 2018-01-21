727a091871222fb23c23110239e8b9e5bc4ffe39
Renan Passos <renanpassos@poli.ufrj.br>
2018-01-21 01:41:17 -0200

    Cria a classe Resistores com vistas à generalização do código

--- Combinacao de Resistores/src/Principal.java
+++ Combinacao de Resistores/src/Principal.java
@@ -1,11 +1,11 @@
 
-public class Combinacao {
+public class Principal {
 
 	final static float [] serieE12 = {10, 12, 15, 18, 22, 27, 33, 39, 47, 56, 68, 82, 100, 120, 150, 180, 220, 270, 330};  
 	final static float razaoIdeal1 = (float) (0.8/5.0);
 	final static float razaoIdeal2 = (float) (3.0/5.0);
 	final static float tolerancia = (float) 0.01;	
-	final float [] razao = {0.8, 1.2, 3.0}; //Valores que expressam as relações de resistências
+	//final float [] razao = {0.8, 1.2, 3.0}; //Valores que expressam as relações de resistências
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
