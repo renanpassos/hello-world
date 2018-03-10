bad8144daf1f5c3f2fd8b698407dbeca7bbf219c
renanrms <renanpassos@poli.ufrj.br>
2018-03-10 14:26:40 -0300

    Adiciona 'aprendendo.sh' com comandos e comentários para aprender shell scripting.

---
+++ Aprendendo Shell/aprendendo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Podemos quase sempre acrescentar comentários escrevendo um '#' na frente
+# Mas existe uma exceção: quando digtamos '#!' na primeira linha
+# O que vem imediatamente depois será a caminho do programa que interpretará este script
+
+
